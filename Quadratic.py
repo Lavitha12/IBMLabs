3f6cc884afb1c7547b1835e14beddb6796411c44
Lavitha.K <59301292+Lavitha12@users.noreply.github.com>
2020-02-27 16:17:18 +0530

    Create Quadratic.py

---
+++ Quadratic.py
@@ -0,0 +1,8 @@
+import cmath
+a=int(input())
+b=int(input())
+c=int(input())
+d=(b**2)-(4*a*c)
+s1=(-b - cmath.sqrt(d))/(2*a)
+s2=(-b + cmath.sqrt(d))/(2*a)
+print("the solution are {0} and {1}".format(s1,s2))
